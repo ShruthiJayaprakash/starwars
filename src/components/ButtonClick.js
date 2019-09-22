66db2f6461ed2a8b7c77a4ca0e8cb5aa9e234c40
ShruthiJayaprakash <33113215+ShruthiJayaprakash@users.noreply.github.com>
2019-09-22 16:57:57 +0400

    added button clicks

--- src/components/ButtonClick.js
+++ src/components/ButtonClick.js
@@ -7,7 +7,26 @@ export default class ButtonClick extends Component {
     axios
       .get("https://swapi.co/api/films")
       .then(response => {
-        console.log("response.data" + JSON.stringify(response));
+        // console.log("response.data" + JSON.stringify(response));
+        //results.opening_crawl
+        let maxCrawl = Math.max(
+          ...response.data.results.map(el => el.opening_crawl.length)
+        );
+        console.log("maxCrawl" + JSON.stringify(maxCrawl));
+        let maxCrawlTitle = response.data.results.filter(
+          el => el.opening_crawl.length === maxCrawl
+        );
+        console.log("maxCrawlTitle" + JSON.stringify(maxCrawlTitle[0].title));
+      })
+      .catch(err => {
+        console.log("err" + err);
+      });
+
+    axios
+      .get("https://swapi.co/api/people")
+      .then(response => {
+        // console.log("response.data" + JSON.stringify(response));
+        //results.films
       })
       .catch(err => {
         console.log("err" + err);
